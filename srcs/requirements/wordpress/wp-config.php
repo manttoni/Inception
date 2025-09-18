fe1f8d8fee49b4880b363b62c412c6da2ac0e10c
Anttoni Maula <manttoni@gmail.com>
2025-09-18 16:51:41 +0300

    passwords

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -1,5 +1,5 @@
-define('DB_NAME', 'wordpress_db');
-define('DB_USER', 'wp_user');
-define('DB_PASSWORD', 'wp_password');
-define('DB_HOST', 'mariadb:3306');
+define('DB_NAME', getenv('MARIADB_DATABASE') ?: 'db');
+define('DB_USER', getenv('MARIADB_USER') ?: 'uname');
+define('DB_PASSWORD', getenv('MARIADB_PASSWORD') ?: 'password');
+define('DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'mariadb:3306');
 
