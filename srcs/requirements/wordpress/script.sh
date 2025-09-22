f77af065a0c83fc3d46e582b4e0bd7537ba6d44d
Anttoni Maula <manttoni92@gmail.com>
2025-09-22 16:19:48 +0300

    password is set correctly maybe

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -3,35 +3,39 @@ cd /var/www/html
 if [ ! -f wp-config.php ]; then
 	echo "Creating wp-config.php"
 
-	mariadb-admin ping --protocol=tcp --host=mariadb -u $MDB_USER --password=$MDB_USER_PWD --wait >/dev/null 2>/dev/null
+	mariadb-admin ping 
+		--protocol=tcp 
+		--host=$WORDPRESS_DB_HOST -u $MARIADB_USER 
+		--password=$MARIADB_PASSWORD 
+		--wait 
 
 	wp core download    --allow-root \
-						--version='latest'
+		--version='latest'
 
 	wp config create    --allow-root \
-						--dbname=$MDB_NAME \
-						--dbuser=$MDB_USER \
-						--dbpass=$MDB_USER_PWD \
-						--dbhost=mariadb:3306
+		--dbname=$MARIADB_USER \
+		--dbuser=$MARIADB_USER \
+		--dbpass=$MARIADB_PASSWORD \
+		--dbhost=$WORDPRESS_DB_HOST
 
 	wp core install     --allow-root \
-						--skip-email \
-						--url=$WP_URL \
-						--title=$WP_TITLE \
-						--admin_user=$WP_ADMIN \
-						--admin_email=$WP_ADMIN_MAIL \
-						--admin_password=$WP_ADMIN_PWD
+		--skip-email \
+		--url=$WP_URL \
+		--title=$WP_TITLE \
+		--admin_user=$WP_ADMIN \
+		--admin_email=$WP_ADMIN_MAIL \
+		--admin_password=$WP_ADMIN_PWD
 
 
 	wp user create      --allow-root \
-						--path=/var/www/html \
-						$WP_USER $WP_USER_MAIL \
-						--user_pass=$WP_USER_PWD
+		--path=/var/www/html \
+		$WP_USER $WP_USER_MAIL \
+		--user_pass=$WP_USER_PWD
 fi
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+#curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+#chmod +x wp-cli.phar
+#./wp-cli.phar core download --allow-root
+#./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
+#./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
 php-fpm8.2 -F
